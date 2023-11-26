428b388ed4c6e9a6cc984732a838180822659d52
AdeBeeY <adewusiadebayo@gmail.com>
2023-11-26 17:35:36 +0100

    Add button, title
    For user to input desired number of grid

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const btnElement = document.querySelector('.js-btn');
 const containerElement = document.querySelector('.js-container');
 
-  for (let i = 1; i < 16*16; i++) {
+function generateGrid(row, column) {
+  for (let i = 1; i <= row*column; i++) {
     const cellElement = document.createElement('div');
     cellElement.classList.add('cell');
     containerElement.appendChild(cellElement);
-
+  
     cellElement.addEventListener('mouseenter', () => {
       cellElement.style.backgroundColor = 'black';
     });
@@ -13,3 +15,26 @@ const containerElement = document.querySelector('.js-container');
       cellElement.style.backgroundColor = 'yellow';
     });
   };
+  containerElement.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
+};
+
+let rows = 16;
+let columns = 16;
+generateGrid(rows, columns);
+
+btnElement.addEventListener('click', () => {
+  let userInput = Number(prompt('Enter the number of columns:', 10));
+  
+  containerElement.textContent = '';
+
+  if(userInput > 30) {
+    alert('Error! Maximum input should be 30');
+  } else if (!isNaN(userInput) && userInput > 0) {
+    rows = userInput;
+    columns = userInput;
+
+    generateGrid(rows, columns);
+  } else {
+    alert('Please, enter a positive number');
+  }
+});
